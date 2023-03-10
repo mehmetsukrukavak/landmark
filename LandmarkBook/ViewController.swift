51bc1d93c0dc1d0ac7f88f0c6a2e9538732dfe5c
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2023-03-10 12:53:07 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Mehmet Sukru Kavak on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
